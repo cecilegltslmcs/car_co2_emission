3d29d6d43d2c69fd82d73ea9afc03245f631b21c
Cecile Guillot <cecile@guillot-salmacis.com>
2023-11-04 23:16:00 +0100

    add shebang + module docstring

--- predict.py
+++ predict.py
@@ -1,3 +1,18 @@
+#!/usr/bin/python3
+"""
+CO2 Emission Predictor API
+
+This module provides a Flask API for predicting CO2 emissions
+of a car based on the received JSON data. It uses a pre-trained
+RandomForest Regressor model stored in "random_forest.bin"
+for making predictions.
+
+Usage:
+    You can use this API to send JSON data containing car information
+    and receive a prediction of CO2 emissions in response.
+
+"""
+
 import pickle
 import numpy as np
 
@@ -5,9 +20,9 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 
-model_file = "random_forest.bin"
+MODEL_FILE = "random_forest.bin"
 
-with open(model_file, "rb") as f_in:
+with open(MODEL_FILE, "rb") as f_in:
     dv, model = pickle.load(f_in)
 
 
@@ -29,12 +44,12 @@ def predict():
         A dictionary containing the predicted CO2 emission.
     """
     car = request.get_json()
-    
-    X = dv.transform([car])
-    y_pred = model.predict(X)
+
+    car_data = dv.transform([car])
+    y_pred = model.predict(car_data)
     y_pred = np.expm1(y_pred).round(2)
     pred = float(y_pred)
-    
+
     return jsonify(pred)
 
 if __name__ == "__main__":
