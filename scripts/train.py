4eea43168f4d309e9449dd72319f8d24417ae7ea
Cecile Guillot <cecile@guillot-salmacis.com>
2023-11-04 23:10:36 +0100

    using linter for correction

--- scripts/train.py
+++ scripts/train.py
@@ -1,3 +1,19 @@
+"""
+RandomForestRegressorTrainer Module
+
+This module provides functionality to train a RandomForest Regressor
+using data from a Pandas DataFrame. It is
+designed to assist in building regression models
+for predictive analysis.
+
+Usage:
+    You can use this module to train a RandomForest Regressor model by providing
+    a Pandas DataFrame containing your feature variables and target variable.
+"""
+
+# utils
+import pickle
+
 # libraries used for data exploration
 import numpy as np
 import pandas as pd
@@ -8,13 +24,11 @@ from sklearn.feature_extraction import DictVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
-# utils
-import pickle
 
-PATH = "data/dataset.csv"
+INPUT_DATASET = "data/dataset.csv"
 OUTPUT_FILE = "random_forest.bin"
 
-def clean_and_process_dataset(PATH:str) -> pd.DataFrame:
+def clean_and_process_dataset(path_file:str)-> pd.DataFrame:
     """
     Load a dataset from the given file path and return a cleaned version.
 
@@ -28,14 +42,13 @@ def clean_and_process_dataset(PATH:str) -> pd.DataFrame:
     pandas.DataFrame
         A Pandas DataFrame containing the cleaned dataset.
     """
-    df = pd.read_csv(PATH)
+    df = pd.read_csv(path_file)
 
     # Remove rows with 'electric' in the 'energy' column
     df = df[df["energy"] != 'electric']
 
     # Define lists of categorical and numerical columns
     numerical = list(df.select_dtypes(exclude=['object']).columns)
-    
     # Fill missing values in numerical columns with their medians
     df[numerical] = df[numerical].fillna(df[numerical].median())
 
@@ -45,16 +58,16 @@ def clean_and_process_dataset(PATH:str) -> pd.DataFrame:
 
     # Remove unwanted columns
     columns_to_remove = [
-        "electrical_nominal_power", 
-        "elect_conso", 
-        "electrical_autonomy", 
+        "electrical_nominal_power",
+        "elect_conso",
+        "electrical_autonomy",
         "city_electrical_autonomy"
         ]
     df = df.drop(columns=columns_to_remove, axis=1)
-    
+
     return df
 
-def split_and_prepare_data(df:pd.DataFrame) -> (pd.DataFrame, pd.DataFrame, np.array, np.array):
+def split_and_prepare_data(dataset:pd.DataFrame)-> (pd.DataFrame, pd.DataFrame, np.array, np.array):
     """
     Split the dataset into training and testing sets and prepare them for modeling.
 
@@ -74,20 +87,21 @@ def split_and_prepare_data(df:pd.DataFrame) -> (pd.DataFrame, pd.DataFrame, np.a
     numpy.ndarray
         Target values for the testing data.
     """
-    df_train, df_test = train_test_split(df, test_size=0.2, random_state=42)
+    train_dataset, test_dataset = train_test_split(dataset, test_size=0.2, random_state=42)
 
-    df_train = df_train.reset_index(drop=True)
-    df_test = df_test.reset_index(drop=True)
+    train_dataset = train_dataset.reset_index(drop=True)
+    test_dataset = test_dataset.reset_index(drop=True)
 
-    y_train = df_train["co2_emission"].values
-    y_test = df_test["co2_emission"].values
+    train_values = df_train["co2_emission"].values
+    test_values = df_test["co2_emission"].values
 
     del df_train['co2_emission']
     del df_test['co2_emission']
 
-    return df_train, df_test, y_train, y_test
+    return train_dataset, test_dataset, train_values, test_values
 
-def train_random_forest_model(df:pd.DataFrame, y_train:np.array) -> (DictVectorizer, RandomForestRegressor):
+def train_random_forest_model(train_dataset:pd.DataFrame,
+                              train_values:np.array)-> (DictVectorizer, RandomForestRegressor):
     """
     Train a Random Forest regression model and return a DictVectorizer and the trained model.
 
@@ -104,9 +118,9 @@ def train_random_forest_model(df:pd.DataFrame, y_train:np.array) -> (DictVectori
         A tuple containing a DictVectorizer for transforming the data and a trained Random
         Forest model for regression.
     """
-    train_dicts = df.to_dict(orient='records')
-    dv = DictVectorizer(sparse=False)
-    X_train = dv.fit_transform(train_dicts)
+    train_dicts = train_dataset.to_dict(orient='records')
+    dict_vectorizer = DictVectorizer(sparse=False)
+    train_dicts_vectorized = dict_vectorizer.fit_transform(train_dicts)
 
     rf = RandomForestRegressor(
         max_depth=10,
@@ -115,12 +129,15 @@ def train_random_forest_model(df:pd.DataFrame, y_train:np.array) -> (DictVectori
         random_state=42,
         n_jobs=-1
     )
-    rf.fit(X_train, y_train)
-    
-    return dv, rf
+
+    rf.fit(train_dicts_vectorized, train_values)
+
+    return dict_vectorizer, rf
 
 
-def predict_co2_emission(dv: DictVectorizer, model: RandomForestRegressor, df: pd.DataFrame) -> np.array:
+def predict_co2_emission(dict_vectorizer: DictVectorizer,
+                         random_forest_regressor: RandomForestRegressor,
+                         test_dataframe: pd.DataFrame)-> np.array:
     """
     Predict CO2 emissions of cars and return the predicted values.
 
@@ -138,14 +155,14 @@ def predict_co2_emission(dv: DictVectorizer, model: RandomForestRegressor, df: p
     numpy.ndarray
         An array containing the predicted CO2 emissions.
     """
-    test_dicts = df.to_dict(orient='records')
-    X_test = dv.transform(test_dicts)
-    y_pred = model.predict(X_test)
-    
-    return y_pred
+    test_dicts = test_dataframe.to_dict(orient='records')
+    test_dataset = dict_vectorizer.transform(test_dicts)
+    predictions = random_forest_regressor.predict(test_dataset)
+
+    return predictions
 
 
-data = clean_and_process_dataset(PATH)
+data = clean_and_process_dataset(INPUT_DATASET)
 df_train, df_test, y_train, y_test = split_and_prepare_data(data)
 dv, model = train_random_forest_model(df_train, y_train)
 y_pred = predict_co2_emission(dv, model, df_test)
